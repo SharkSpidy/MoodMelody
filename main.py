c2ecc992308ed520fa375a296b6a0cefc20cacab
SharkSpidy <josephshibuwork@gmail.com>
2025-03-26 18:02:17 +0530

    done with forms stuff

--- main.py
+++ main.py
@@ -1,14 +1,22 @@
-from flask import Flask
+from flask import Flask, render_template, request
+from flask_wtf import FlaskForm
+from wtforms import StringField, SubmitField
+from wtforms.validators import DataRequired
 
 app = Flask(__name__)
+app.config['SECRET_KEY'] = 'JJ@07092005'
 
-@app.route('/')
-def home():
-    return "Helloo!!"
+class NameForm(FlaskForm):
+    name = StringField("Enter Your Name:", validators=[DataRequired()])
+    submit = SubmitField("Submit")
 
-@app.route('/about')
-def about():
-    return "This is the About Page!!"
+@app.route('/form', methods = ['GET', 'POST'])
+def form():
+    form = NameForm()
+    if form.validate_on_submit():
+        name = form.name.data
+        return f"Hello, {name}!"
+    return render_template('form.html',form = form)
 
 if __name__ == '__main__':
     app.run(debug=True)
